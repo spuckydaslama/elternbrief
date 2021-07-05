668c8caeefab48d1ae4b252d46694cfeaacf467d
Daniel Ebert <daniel.ebert@bitgrip.de>
2021-07-05 21:10:46 +0200

    added tailwindcss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Elternbrief</title>
+</svelte:head>
+
+<h1>Willkommen beim Elternbrief-Generator</h1>
+
