31a93184c4d1f0d58b7a95295b44e7395e175ac7
Daniel Ebert <daniel.ebert@bitgrip.de>
2021-07-07 16:44:42 +0200

    added static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import staticadapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,12 +11,18 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: staticadapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+		}),
 	}
 };
 
-export default config;
 // Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 process.env.TAILWIND_MODE = dev ? "watch" : "build";
+
+export default config;
