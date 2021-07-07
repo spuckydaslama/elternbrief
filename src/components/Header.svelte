5390ffb833d4df83e95923ebcc204cb3672836ca
Daniel Ebert <daniel.ebert@bitgrip.de>
2021-07-07 10:04:32 +0200

    basic header/main/footer structure

---
+++ src/components/Header.svelte
@@ -0,0 +1,3 @@
+<nav class='flex justify-center w-full bg-blue-300'>
+	<a class='mx-4 text-lg' href='/'>Home</a>
+</nav>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,17 @@
 <script>
 	import '../app.postcss';
+	import Footer from '../components/Footer.svelte';
+	import Header from '../components/Header.svelte';
 </script>
 
-<slot />
+<div class="p-8 flex flex-col min-h-screen">
+	<header>
+		<Header />
+	</header>
+	<main class='flex-grow'>
+		<slot />
+	</main>
+	<footer>
+		<Footer />
+	</footer>
+</div>
