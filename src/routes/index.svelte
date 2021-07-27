a5ac370e3bb2a25ebda3eafce146bdb99c049313
Sebastian Kurt <sebastian.kurt@gmail.com>
2021-07-27 22:24:21 +0200

    kurzanleitung draft
    #6 draft

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,4 +7,11 @@
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8">Willkommen beim Elternbrief-Generator</h1>
+<p>Im Herbst sind Bundestagswahlen. Die Kinder haben wenig Einfluss darauf. Trotzdem geht es zu großen Teilen um ihre Zukunft. Wir wollen ihnen eine Stimme verleihen.</p>
+<ul>
+	<li>Passe die Briefvorlage so an, um im Namen deiner Kinder eine Botschaft an Ältere zu senden.</li>
+	<li>Wähle Themen aus, auf die die Angesprochenen reagieren, sich angesprochen fühlen.</li>
+	<li>Versende/Teile den fertigen Brief auf einem passenden Weg - E-Mail, WhatsApp, per Post oder wie es wirklich ankommt.</li>
+	<li>Falls es passt - Sprich bei der nächsten passenden Gelegenheit mit den Angeschriebenen.</li>
+</ul>
 <ElternbriefGenerator />
