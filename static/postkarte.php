af76b354383739aca311feb92711dd5462e53bb0
Daniel Ebert <daniel.ebert@bitgrip.de>
2021-08-27 11:19:24 +0200

    #12 datenschutz - die postkartenaufträge sollten nicht im web sichtbar sein

--- static/postkarte.php
+++ static/postkarte.php
@@ -1,5 +1,5 @@
 <?php
 $entityBody = file_get_contents('php://input');
-$file = 'postkarten.txt';
+$file = '../ebg-postkarten/postkarten.txt';
 file_put_contents($file, $entityBody . PHP_EOL, FILE_APPEND | LOCK_EX);
 ?>
