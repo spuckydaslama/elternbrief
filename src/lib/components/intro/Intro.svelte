139f57bad592b910db9dd0cde5763850be2e932e
Daniel Ebert <daniel.ebert@bitgrip.de>
2021-09-02 13:58:21 +0200

    #13 Postkartenmotiv wird im Intro angezeigt

---
+++ src/lib/components/intro/Intro.svelte
@@ -0,0 +1,25 @@
+<h1 class="text-4xl text-center my-8">Gib deinem Kind eine Stimme</h1>
+<section class='md:grid md:grid-cols-2 lg:grid-cols-3'>
+	<div class='lg:col-span-2'>
+		<p>
+			Im Herbst sind Bundestagswahlen. Die Kinder haben wenig Einfluss darauf. Trotzdem geht es zu
+			großen Teilen um ihre Zukunft. Wir wollen ihnen eine Stimme verleihen.
+		</p>
+		<ul class="list-disc list-inside ml-2">
+			<li>
+				Passe die Briefvorlage so an, um im Namen deiner Kinder eine Botschaft an Ältere zu senden.
+			</li>
+			<li>Wähle Themen aus, auf die die Angesprochenen reagieren, sich angesprochen fühlen.</li>
+			<li>
+				Versende/Teile den fertigen Brief auf einem passenden Weg - E-Mail, WhatsApp, per Post oder
+				wie es wirklich ankommt.
+			</li>
+			<li>
+				Falls es passt - Sprich bei der nächsten passenden Gelegenheit mit den Angeschriebenen.
+			</li>
+		</ul>
+	</div>
+	<div class='md:mt-0 mt-2 md:ml-2 self-center'>
+		<img class='border md:max-h-64' src='postkartenmotiv.jpg' alt='Postkartenmotiv' />
+	</div>
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,14 @@
 <script>
 	import ElternbriefGenerator from '$lib/components/generator/ElternbriefGenerator.svelte';
 	import Faq from '$lib/components/faq/Faq.svelte';
+	import Intro from '$lib/components/intro/Intro.svelte';
 </script>
 
 <svelte:head>
 	<title>Elternbrief</title>
 </svelte:head>
 
-<section>
-	<h1 class="text-4xl text-center my-8">Gib deinem Kind eine Stimme</h1>
-	<p>
-		Im Herbst sind Bundestagswahlen. Die Kinder haben wenig Einfluss darauf. Trotzdem geht es zu
-		großen Teilen um ihre Zukunft. Wir wollen ihnen eine Stimme verleihen.
-	</p>
-	<ul class="list-disc list-inside ml-2">
-		<li>
-			Passe die Briefvorlage so an, um im Namen deiner Kinder eine Botschaft an Ältere zu senden.
-		</li>
-		<li>Wähle Themen aus, auf die die Angesprochenen reagieren, sich angesprochen fühlen.</li>
-		<li>
-			Versende/Teile den fertigen Brief auf einem passenden Weg - E-Mail, WhatsApp, per Post oder
-			wie es wirklich ankommt.
-		</li>
-		<li>Falls es passt - Sprich bei der nächsten passenden Gelegenheit mit den Angeschriebenen.</li>
-	</ul>
-</section>
+<Intro />
 <ElternbriefGenerator />
 <Faq />
 <section>
