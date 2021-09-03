e90ba8c77efb478835cf6620d56cbf2253a985ae
Daniel Ebert <daniel.ebert@bitgrip.de>
2021-09-03 15:40:29 +0200

    #23 Intro angepasst, why what how

--- src/lib/components/intro/Intro.svelte
+++ src/lib/components/intro/Intro.svelte
@@ -1,22 +1,32 @@
 <h1 class="text-4xl text-center my-8">Gib deinem Kind eine Stimme</h1>
 <section class="md:grid md:grid-cols-2 lg:grid-cols-3">
 	<div class="lg:col-span-2">
-		<p>
-			Im Herbst sind Bundestagswahlen. Die Kinder haben wenig Einfluss darauf. Trotzdem geht es zu
-			großen Teilen um ihre Zukunft. Wir wollen ihnen eine Stimme verleihen.
-		</p>
-		<ul class="list-disc list-inside ml-2">
+		<div>
+			<h2 class="inline caps-small text-indigo-800 text-2xl mr-1">Warum?</h2>
+			<p class="inline">
+				Im Herbst sind Bundestagswahlen. Die Kinder haben wenig Einfluss darauf. Trotzdem geht es zu
+				großen Teilen um ihre Zukunft. Wir wollen ihnen eine Stimme verleihen.
+			</p>
+		</div>
+		<div>
+			<h2 class="inline caps-small text-indigo-800 text-2xl mr-1">Was?</h2>
+			<p class="inline">
+				Schicke im Namen deiner Kinder Postkarten (ja, du darfst mehrfach) an Großeltern, Tanten und
+				Onkel.
+			</p>
+		</div>
+		<h2 class="block caps-small text-indigo-800 text-2xl mr-1">Wie?</h2>
+		<ul class="list-disc list-inside ml-3">
 			<li>
 				Passe die Briefvorlage so an, um im Namen deiner Kinder eine Botschaft an Ältere zu senden.
+				Verwende die Namen, dann wirkt es viel persönlicher!
 			</li>
 			<li>Wähle Themen aus, auf die die Angesprochenen reagieren, sich angesprochen fühlen.</li>
 			<li>
-				Versende/Teile den fertigen Brief auf einem passenden Weg - E-Mail, WhatsApp, per Post oder
-				wie es wirklich ankommt.
+				Versende den Text als Postkarte (kostenlos!) oder auf einem passenden Weg - E-Mail,
+				WhatsApp. Am besten so wie es wirklich ankommt.
 			</li>
-			<li>
-				Falls es passt - Sprich bei der nächsten passenden Gelegenheit mit den Angeschriebenen.
-			</li>
+			<li>Sprich mit den Angeschriebenen.</li>
 		</ul>
 	</div>
 	<div class="md:mt-0 mt-2 md:ml-2 self-center">
