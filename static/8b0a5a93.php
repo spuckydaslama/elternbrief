03819242c8c24aa35ef26c7e0223e74fe4b0a2a3
spuckydaslama <daniel.3bert@gmail.com>
2024-11-27 11:50:36 +0100

    add timestamp and determine filename based on hostname

--- static/8b0a5a93.php
+++ static/8b0a5a93.php
@@ -3,14 +3,18 @@
 $userAgent = $_SERVER['HTTP_USER_AGENT'];
 $referer = $_SERVER['HTTP_REFERER'];
 $hostname = $_SERVER['HTTP_HOST'];
+// add the time of the request
+$timestamp = date('Y-m-d H:i:s');
 
 // create a json string with all the information
 $entityBody = json_encode(array(
+    'timestamp' => $timestamp,
     'userAgent' => $userAgent,
     'referer' => $referer,
-    'hostname' => $hostname
 ));
 
-$file = "../ebg-tracking/text.txt";
+// create a filename based on the hostname that is either ebg-stage.pirsh.de or www.stimme-fuer-kinder.de
+$filename = $hostname === 'ebg-stage.pirsh.de' ? 'ebg-stage' : 'ebg-prod';
+
+$file = "../ebg-tracking/${filename}.txt";
 file_put_contents($file, $entityBody . PHP_EOL, FILE_APPEND | LOCK_EX);
-?>
--- static/postkarte.php
+++ static/postkarte.php
@@ -2,4 +2,3 @@
 $entityBody = file_get_contents('php://input');
 $file = '../ebg-postkarten/postkarten.txt';
 file_put_contents($file, $entityBody . PHP_EOL, FILE_APPEND | LOCK_EX);
-?>
