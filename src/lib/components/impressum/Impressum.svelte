0897a9d9571c16d45cb8d24b67fbb55629a1dcbf
Sebastian Kurt <sebastian.kurt@gmail.com>
2024-11-29 13:28:06 +0100

    Update Impressum.svelte
    new address

--- src/lib/components/impressum/Impressum.svelte
+++ src/lib/components/impressum/Impressum.svelte
@@ -3,8 +3,7 @@
 <section class="m-2">
 	<h2 class="mb-4 mt-4 text-2xl">Impressum</h2>
 	<p>Sebastian Kurt</p>
-	<p>Rigaer Straße 17, 10249 Berlin</p>
-	<p>+493089785-9247</p>
+	<p>Steinbachstraße 98, 12489 Berlin</p>
 	<p>
 		<a class="text-blue-700 underline" href="mailto:sebastian.kurt@gmail.com">
 			sebastian.kurt@gmail.com
