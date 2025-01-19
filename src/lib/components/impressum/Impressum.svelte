0e08e9ebb3bc672c772dad711c829b69dc5be1a7
spuckydaslama <daniel.3bert@gmail.com>
2025-01-19 16:12:09 +0100

    fix impressum

--- src/lib/components/impressum/Impressum.svelte
+++ src/lib/components/impressum/Impressum.svelte
@@ -3,7 +3,7 @@
 <section class="mt-8 bg-[#a5ca61] px-8">
 	<h2 class="mb-4 mt-4 text-2xl">Impressum</h2>
 	<p>Sebastian Kurt</p>
-	<p>Rigaer Straße 17, 10249 Berlin</p>
+	<p>Steinbachstraße 98, 12489 Berlin</p>
 	<p>
 		<a class="text-blue-700 underline" href="mailto:sebastian.kurt@gmail.com">
 			sebastian.kurt@gmail.com
