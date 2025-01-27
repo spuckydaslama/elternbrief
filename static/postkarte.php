73702840be07151ad6e23c3374d05bb584ad8d26
spuckydaslama <daniel.3bert@gmail.com>
2025-01-27 10:29:14 +0100

    hostname based filenames

--- static/postkarte.php
+++ static/postkarte.php
@@ -1,4 +1,7 @@
 <?php
 $entityBody = file_get_contents('php://input');
-$file = '../ebg-postkarten/postkarten.txt';
+
+$hostname = $_SERVER['HTTP_HOST'];
+$filename = $hostname === 'ebg-stage.pirsh.de' ? 'ebg-stage-postkarten' : 'ebg-prod-portkarten';
+$file = '../ebg-postkarten/${filename}.txt';
 file_put_contents($file, $entityBody . PHP_EOL, FILE_APPEND | LOCK_EX);
