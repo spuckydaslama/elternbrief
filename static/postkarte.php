34295b4990ddb5c6cae68f7ceb70535c8225de3e
spuckydaslama <daniel.3bert@gmail.com>
2025-01-27 10:32:08 +0100

    fix server script

--- static/postkarte.php
+++ static/postkarte.php
@@ -3,5 +3,5 @@ $entityBody = file_get_contents('php://input');
 
 $hostname = $_SERVER['HTTP_HOST'];
 $filename = $hostname === 'ebg-stage.pirsh.de' ? 'ebg-stage-postkarten' : 'ebg-prod-portkarten';
-$file = '../ebg-postkarten/${filename}.txt';
+$file = "../ebg-postkarten/${filename}.txt";
 file_put_contents($file, $entityBody . PHP_EOL, FILE_APPEND | LOCK_EX);
